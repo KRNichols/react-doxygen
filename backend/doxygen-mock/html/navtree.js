0c7d8784189eb00dc00cbbbff4ee7514787c96bf
Kevin Nichols <48831113+KRNichols@users.noreply.github.com>
2026-08-26 21:36:24 -0400

    Add mock Doxygen CSS and navtree.

---
+++ backend/doxygen-mock/html/navtree.js
@@ -0,0 +1,22 @@
+var NAVTREE =
+[
+  [ "F/A-18 Mission Software", "index.html", [
+    [ "Main Page", "index.html", null ],
+    [ "Modules", "modules.html", [
+      [ "Flight Control", "group__flight.html", null ],
+      [ "Radar", "group__radar.html", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "FlightControl", "class_flight_control.html", null ],
+      [ "RadarSuite", "class_radar_suite.html", null ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "fcs.h", "fcs_8h.html", null ],
+      [ "radar.h", "radar_8h.html", null ]
+    ] ]
+  ] ]
+];
+var NAVTREEINDEX =
+[
+"annotated.html"
+];
